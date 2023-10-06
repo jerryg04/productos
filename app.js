d5631a90ad7f68755142f714f7f36c32d419a783
Jerry Gmz <gomez.gergil@gmail.com>
2023-10-06 10:35:27 -0600

    gitignose se agrego .env

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use(
 );
 
 //PORT
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//     connectionString: process.env.DB_POSTGRESQL_URL
+// });
 const pool = new Pool({
-    user : "postgres",
-    password: "postgres",
-    host: "localhost",
-    port: 5432,
-    database: "my_pgdb"
+    user : process.env.DB_POSTGRESQL_USER,
+    password: process.env.DB_POSTGRESQL_PASSWORD,
+    host: process.env.DB_POSTGRESQL_HOST,
+    port: process.env.DB_POSTGRESQL_PORT,
+    database: process.env.DB_POSTGRESQL_NAME
 });
 
 module.exports = pool;
